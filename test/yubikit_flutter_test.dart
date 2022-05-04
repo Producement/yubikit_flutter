3f51ec4374b845eb7b6ee241015dd66abb58745d
Maido Käära <maido@producement.com>
2022-05-04 11:30:27 +0300

    Fix analyze issues

--- test/yubikit_flutter_test.dart
+++ test/yubikit_flutter_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:yubikit_flutter/piv/piv_session.dart';
 import 'package:yubikit_flutter/yubikit_flutter.dart';
 
 void main() {
@@ -9,6 +10,9 @@ void main() {
 
   setUp(() {
     channel.setMockMethodCallHandler((MethodCall methodCall) async {
+      if(methodCall.method == "pivReset"){
+        return null;
+      }
       return '42';
     });
   });
@@ -17,7 +21,8 @@ void main() {
     channel.setMockMethodCallHandler(null);
   });
 
-  test('connect', () async {
-    //expect(await YubikitFlutter.connect(), '42');
+  test('reset', () async {
+    YubikitFlutterPivSession pivSession = YubikitFlutter.pivSession();
+    await pivSession.reset();
   });
 }
